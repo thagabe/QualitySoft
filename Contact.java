6689bc0b03517c65fa8e352a8453fbf2a13bf917
Gabe Rodriguez <thagabe@users.noreply.github.com>
2016-05-31 11:52:37 -0700

    Small Changes to Javadoc

--- Contact.java
+++ Contact.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 /**
  * One Object of the class Contact represents one person's contact information
- **/
+ */
 public class Contact {
 	
 	private String firstName;
@@ -13,7 +13,7 @@ public class Contact {
 	
 	/**
 	 * Set private variables to default values
-	 **/
+	 */
 	public Contact(){
 		firstName = "";
 		firstName = "";
@@ -26,7 +26,7 @@ public class Contact {
 	
 	/**
 	 * Sets private variables to passed arguments 
-	 **/
+	 */
 	public Contact(String newFirstName, String newLastName, String newStreetAddress, String newEmailAddress, String newPhoneNumber, String newNotes){
 		firstName = newFirstName;
 		lastName = newLastName;
@@ -38,57 +38,57 @@ public class Contact {
 	
 	/**
 	 * Sets firstName to newFirstName
-	 **/
+	 */
 	public void setFirstName(String newFirstName) { firstName = newFirstName; }
 	/**
 	 * Sets lastName to newFirstName
-	 **/
+	 */
 	public void setLastName(String newLastName){ lastName = newLastName; }
 	/**
 	 * Sets streetName to newStreetName
-	 **/
+	 */
 	public void setStreetAddress(String newStreetAddress){ streetName = newStreetName; }
 	/**
 	 * Sets emailAddress to newEmailAddress
-	 **/
+	 */
 	public void setEmailAddress(String newEmailAddress){ emailAddress = newEmailAddress; }
 	/**
 	 * Sets phoneNumber to newPhoneNumber
-	 **/
+	 */
 	public void setPhoneNumber(String newPhoneNumber){ phoneNumber = newPhoneNumber; }
 	/**
 	 * Sets notes to newNotes
-	 **/
+	 */
 	public void setNotes(String newNotes){ notes = newNotes; }
 	
 	/**
 	 * Returns value of firstName
-	 **/
+	 */
 	public String getFirstName() { return firstName; }
 	/**
 	 * Returns value of lastName
-	 **/
+	 */
 	public String getLastName() { return lastName; }
 	/**
 	 * Returns value of streetAddress
-	 **/
+	 */
 	public String getStreetAddress() { return streetAddress; }
 	/**
 	 * Returns value of emailAddress
-	 **/
+	 */
 	public String getEmailAddress() { return emailAddress; }
 	/**
 	 * Returns value of phoneNumber
-	 **/
+	 */
 	public String getPhoneNumber() { return phoneNumber; }
 	/**
 	 * Returns value of notes
-	 **/
+	 */
 	public String getNotes() { return notes; }
 
 	/**
 	 * Returns formatted string displaying all characteristics of Contact
-	 **/
+	 */
 	private String toString(){
 		return (firstName + "\t\t\t" + lastName + "\t\t\t" + streetAddress + "\t\t\t" + emailAddress + 
 		"\t\t\t" + phoneNumber + "\t\t\t" + notes);
