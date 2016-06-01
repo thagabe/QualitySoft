804c37ef1fcec49d5b0c8202b341a2e3638e10bb
Gabe Rodriguez <thagabe@users.noreply.github.com>
2016-05-31 21:49:37 -0700

    Update Main.java
    Changes made to reflect the changes in the method's return 
    
    //TODO
    We must incorporate use case #4 and #5
    Also the user input must be added

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
-public Main(){
-  public static main (String args[]) {
-  MyContactList list1 = new MyContactList();
-  list1.newContact("Alex", "Lui", "1729 Madras st", "alex.lui@gmail.com", "664-543-4122", "Manager");
-  list1.newContact("Gabe", "Rodriguez", "1623 Van Halen Rd", "g.rodriguez@gmail.com", "650-456-7890", "Coder");
-  list1.sort();
-  System.out.print(list1.fullList());
-  System.out.print(list1.search("lui"));
-  
-  }
+public class Tester {
+
+	public static void main(String[] args) {
+		MyContactList list1 = new MyContactList();
+		list1.newContact("Alex", "Lui", "1729 Madras st", "alex.lui@gmail.com", "664-543-4122", "Manager");
+		list1.newContact("Gabe", "Rodriguez", "1623 Van Halen Rd", "g.rodriguez@gmail.com", "650-456-7890", "Coder");
+		list1.sortArray();
+		for (Contact i : list1.getArrayListForPrint()) {
+			System.out.println(i.toString());
+		}
+
+		for (Contact j : list1.searchArray("Lui")) {
+			System.out.println(j.toString());
+		}
+	}
+
 }
