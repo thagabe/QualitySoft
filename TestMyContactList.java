bc32c6c15bff87ee9f57b367e81543046eb8961e
bongobl <alexb.lui@gmail.com>
2016-06-13 23:57:41 -0700

    Update TestMyContactList.java

--- TestMyContactList.java
+++ TestMyContactList.java
@@ -1,7 +1,9 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 
