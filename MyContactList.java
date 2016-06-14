65778aecdbb657c6dc28b77188b16d7f330e476f
Gabe Rodriguez <thagabe@users.noreply.github.com>
2016-06-14 11:01:29 -0700

    Update MyContactList.java
    Formatting

--- MyContactList.java
+++ MyContactList.java
@@ -2,11 +2,13 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.io.Serializable;
+
 /**
  * One object of this class represents an ArrayList containing Objects of class
  * Person. AL, GR
  */
-public class MyContactList implements Serializable{
+@SuppressWarnings("serial")
+public class MyContactList implements Serializable {
 
 	private List<Person> contactList; // this stores the list of contacts
 
@@ -23,55 +25,59 @@ public class MyContactList implements Serializable{
 	 * and adding them to the listFile. Checks if at least last name is present
 	 * otherwise it returns false and does not set anything.AL, GR
 	 */
-	public void addContact(Person person){
-		if(!person.getLastName().trim().isEmpty())
+	public void addContact(Person person) {
+		if (!person.getLastName().trim().isEmpty())
 			contactList.add(person);
 	}
 
 	/**
-	 * Formats the object of class MyContactList's data into a string and returns it.AL, GR
+	 * Formats the object of class MyContactList's data into a string and
+	 * returns it.AL, GR
 	 **/
 	public String toString() {
-		
-		//System.out.println("-FIRST NAME-\t\t\t-LAST NAME-\t\t\t-STREETADDRESS-\t\t\t\t-EMAIL ADDRESS-\t\t\t-PHONE #-\t\t\t-NOTES-");
+
+		// System.out.println("-FIRST NAME-\t\t\t-LAST NAME-\t\t\t-STREETADDRESS-\t\t\t\t-EMAIL ADDRESS-\t\t\t-PHONE #-\t\t\t-NOTES-");
 		sortArray();
 		StringBuffer theFormat = new StringBuffer();
 		theFormat.append("__All Contacts__\n");
-		for (Person element: contactList){
+		for (Person element : contactList) {
 			theFormat.append(element.toString() + "\n");
 		}
 		return theFormat.toString();
 	}
-	
+
 	/**
 	 * This method sorts the ArrayList based on the last name in alphabetical
 	 * order from the contacts that are already present in the List, if two
 	 * Contact objects share the same last name then sortArray() will arrange
 	 * them based on alphabetical order on the first names.AL, GR
 	 */
-	
+
 	private void sortArray() {
 		Collections.sort(contactList);
 	}
-	/** 
+
+	/**
 	 * This method returns the current size of contactList
 	 * **/
-	public int getListSize(){
-		return mainList.size();
+	public int getListSize() {
+		return contactList.size();
 	}
+
 	/**
 	 * This method takes in a String which contains the last name to lookup in
 	 * the ArrayList. If an occurrence of the String is encountered it will
 	 * create a new ArrayList with the object(s) appended to this new ArrayList.
-	 * It then returns a Formatted String with the objects' stored information.AL, GR
+	 * It then returns a Formatted String with the objects' stored
+	 * information.AL, GR
 	 */
 	public String searchContactByLastName(String searchLastName) {
-		
+
 		StringBuffer theFormat = new StringBuffer();
 		sortArray();
-		
-		for(Person element: contactList){
-			if(element.getLastName().equals(searchLastName)){
+
+		for (Person element : contactList) {
+			if (element.getLastName().equals(searchLastName)) {
 				theFormat.append(element.toString() + "\n");
 			}
 		}
@@ -79,14 +85,16 @@ public class MyContactList implements Serializable{
 	}
 }
 
-/** Feedback from Alex
- * I think this is very good, I like the way we renamed the methods to describe
- * what they are doing. In the future, I think sortArray() should be private, so that
- * users of this class don't always have to call it before printing the contact list.AL, GR
+/**
+ * Feedback from Alex I think this is very good, I like the way we renamed the
+ * methods to describe what they are doing. In the future, I think sortArray()
+ * should be private, so that users of this class don't always have to call it
+ * before printing the contact list.AL, GR
  * 
  * **/
- 
- /** Feddback from Adina
-  * Use toString() method instead of getArrayListForPrint() to avoid giving the main() the private data structure since it can destroy it
-  * as our director noted.
-  * /
+
+/**
+ * Feddback from Adina Use toString() method instead of getArrayListForPrint()
+ * to avoid giving the main() the private data structure since it can destroy it
+ * as our director noted.
+ **/
