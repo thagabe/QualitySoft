818a7dda6b9bf05ab1a7299ac7af1d0f3219bd24
Gabe Rodriguez <thagabe@users.noreply.github.com>
2016-06-16 06:09:13 -0700

    Update StreetAddress.java

--- StreetAddress.java
+++ StreetAddress.java
@@ -1,10 +1,10 @@
 import java.io.Serializable;
 
 /**
- * Class StreetAddress This class represents one street address object, it has
+ * Class StreetAddress this class represents one street address object, it has
  * fields that specify a person's home address. It has getters, setters, and a
  * toString method, AL
- * **/
+ **/
 @SuppressWarnings("serial")
 public class StreetAddress implements Serializable {
 
@@ -14,10 +14,9 @@ public class StreetAddress implements Serializable {
 	private String stateName;
 	private String zipCode;
 
-	// constructors
 	/**
 	 * Default Constructor This sets all strings to empty, AL
-	 * **/
+	 **/
 	public StreetAddress() {
 		this.streetNumber = "";
 		this.streetName = "";
@@ -29,7 +28,7 @@ public class StreetAddress implements Serializable {
 	/**
 	 * Full Constructor This contains parameters for each varible in the class
 	 * and assigns the class variables to their respective parameter. AL
-	 * **/
+	 **/
 	public StreetAddress(String streetNumber, String streetName,
 			String cityName, String stateName, String zipCode) {
 
@@ -41,46 +40,44 @@ public class StreetAddress implements Serializable {
 
 	}
 
-	// ACCESSORS
 	/**
 	 * returns streetNumber, AL.
-	 * **/
+	 **/
 	String getStreetNumber() {
 		return streetNumber;
 	}
 
 	/**
 	 * returns streetName AL
-	 * **/
+	 **/
 	String getStreetName() {
 		return streetName;
 	}
 
 	/**
 	 * returns cityName, AL
-	 * **/
+	 **/
 	String getCityName() {
 		return cityName;
 	}
 
 	/**
 	 * returns stateName, AL
-	 * **/
+	 **/
 	String getStateName() {
 		return stateName;
 	}
 
 	/**
 	 * returns zipCode AL
-	 * **/
+	 **/
 	String getzipCode() {
 		return zipCode;
 	}
 
-	// MUTATORS
 	/**
 	 * sets streetNumber to parameter, AL
-	 * **/
+	 **/
 	void setStreetNumber(String streetNumber) {
 		this.streetNumber = streetNumber.trim();
 
@@ -88,35 +85,35 @@ public class StreetAddress implements Serializable {
 
 	/**
 	 * sets streetName to parameter, AL
-	 * **/
+	 **/
 	void setStreetName(String streetName) {
 		this.streetName = streetName.trim();
 	}
 
 	/**
 	 * sets cityName to parameter, AL
-	 * **/
+	 **/
 	void setCityName(String cityName) {
 		this.cityName = cityName.trim();
 	}
 
 	/**
 	 * sets stateName to parameter, AL
-	 * **/
+	 **/
 	void setStateName(String stateName) {
 		this.stateName = stateName.trim();
 	}
 
 	/**
 	 * sets zipCode to parameter, AL
-	 * **/
+	 **/
 	void setZipCode(String zipCode) {
 		this.zipCode = zipCode.trim();
 	}
 
 	/**
 	 * returns a string with all the information for a street address, AL
-	 * **/
+	 **/
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
 		if (!this.streetNumber.isEmpty())
